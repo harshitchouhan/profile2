43e703466002e7d33eb90afb438ef7f20a3501f6
Harshit Chouhan <48473127+harshitchouhan@users.noreply.github.com>
2020-02-05 10:27:31 +0530

    Add files via upload

---
+++ assets/contact.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+  <head>
+    <!-- meta -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Harshit Chouhan mobile and front-end developer." />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="description" content="Harshit Chouhan | Front-end Developer " />
+    <meta name="keywords" content="HTML,CSS,JavaScript,frontend,front-end,harshitchouhan,that_webdev" />
+    <meta name="author" content="Harshit Chouhan" />
+    <meta name="abstract" content="Harshit Chouhan - Front-End Developer" />
+    <meta name="designer" content="Harshit Chouhan" />
+    <meta name="rating" content="General" />
+    <meta name="classification" content="Web Programming, Front-End" />
+    <meta name="title" content="Harshit Chouhan | Front-end Developer" />
+    <meta name="theme-color" content="#e53c39" />
+    <link href="favico.png" rel="shortcut icon" />
+
+    <!-- meta -->
+    <title>Contact | Harshit Chouhan</title>
+    <link rel="stylesheet" href="assets/css/global.css" />
+  </head>
+
+  <body>
+    <div class="loader">
+      <div class="loader__inner">
+        <span>Harshit Chouhan</span>
+      </div>
+    </div>
+    <div id="barba-wrapper">
+      <div class="barba-container contact" data-namespace="contact">
+        <!-- header -->
+
+        <header class="section-header">
+          <div class="section__inner">
+            <div class="logo">
+              <a class="logo-front" href="/">Harshit Chouhan</a>
+              <a class="logo-back" href="/">Back to Home</a>
+            </div>
+            <div class="main-menu">
+              <ul>
+                <li><a data-page="work.php" href="work.php">Work</a></li>
+                <li><a data-page="about.php" href="about.php">About</a></li>
+                <li><a data-page="contact.php" href="contact.php">Contact</a></li>
+                <!--                <li><a data-page="blog" href="blog">Blog</a></li>-->
+              </ul>
+            </div>
+            <div class="hamburger">
+              <span class="hamburger__top"></span>
+              <span class="hamburger__center"></span>
+              <span class="hamburger__bottom"></span>
+              <span class="hamburger__close-a"></span>
+              <span class="hamburger__close-b"></span>
+            </div>
+          </div>
+        </header>
+        <!-- header -->
+
+        <section class="section-content">
+          <div class="section__inner">
+            <div class="contact-content">
+              <div class="contact-content__inner">
+                <h1>Get in touch!</h1>
+                <p>
+                  Write me an <span> <a class="no-barba" href="mailto:harshitsinghchouhan2018@gmail.com">email | harshitsinghchouhan2018@gmail.com</a></span
+                  ><br />
+                  Call me on my <span><a class="no-barba" href="tel:+918107136147">phone</a></span
+                  ><br />
+                  Find me on
+                  <span>
+                    <a class="no-barba" href="https://www.instagram.com/that_webdev/?hl=en" target="_blank">instagram</a>
+                  </span>
+                  or
+                  <span>
+                    <a class="no-barba" href="https://www.facebook.com/thatwebdev/" target="_blank">facebook</a>
+                  </span>
+                  
+                </p>
+              </div>
+              <div class="alpha-zio">
+                <div class="alpha-zio__inner"><span></span></div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- footer -->
+        <footer class="section-footer">
+          <div class="section__inner">
+            <ul class="footer-social">
+                <li>
+                  <a href="https://github.com/harshitchouhan" class="github" target="_blank" style="background-image: url(img/github.svg);"></a>
+                </li>
+  
+                <li>
+                  <a href="https://www.facebook.com/thatwebdev/" class="facebook" target="_blank"></a>
+                </li>
+  
+                <li>
+                  <a href="https://www.linkedin.com/in/harshit-chouhan-16a582179/" class="linkedin" target="_blank"></a>
+                </li>
+  
+                <li>
+                  <a href="https://www.instagram.com/that_webdev/?hl=en" class="instagram" target="_blank"></a>
+                </li>
+  
+                <li>
+                  <a href="https://twitter.com/Harshit52344484" class="twitter" target="_blank"></a>
+                </li>
+              </ul>
+            <span>© Harshit Chouhan</span>
+          </div>
+        </footer>
+        <!-- footer -->
+      </div>
+    </div>
+    <div class="cursor">
+      <span class="cursor-alpha"></span>
+    </div>
+    <script>
+      (function(h) {
+        h.className = h.className.replace("no-js", "js");
+      })(document.documentElement);
+    </script>
+    <script src="assets/js/jquery.js" type="text/javascript"></script>
+    <script src="assets/js/main.js" type="text/javascript"></script>
+  </body>
+</html>
---
+++ assets/work.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+  <head>
+    <!-- meta -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Harshit Chouhan mobile and front-end developer." />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta name="description" content="Harshit Chouhan | Front-end Developer " />
+    <meta name="keywords" content="HTML,CSS,JavaScript,frontend,front-end,harshitchouhan,that_webdev" />
+    <meta name="author" content="Harshit Chouhan" />
+    <meta name="abstract" content="Harshit Chouhan - Front-End Developer" />
+    <meta name="designer" content="Harshit Chouhan" />
+    <meta name="rating" content="General" />
+    <meta name="classification" content="Web Programming, Front-End" />
+    <meta name="title" content="Harshit Chouhan | Front-end Developer" />
+    <meta name="theme-color" content="#e53c39" />
+    <link href="favico.png" rel="shortcut icon" />
+
+    <!-- meta -->
+    <title>Works | Harshit CHouhan</title>
+    <link rel="stylesheet" href="assets/css/global.css" />
+  </head>
+
+  <body>
+    <div class="loader">
+      <div class="loader__inner">
+        <span>Harshit CHouhan</span>
+      </div>
+    </div>
+    <div id="barba-wrapper">
+      <div class="barba-container work" data-namespace="work">
+        <!-- header -->
+
+        <header class="section-header">
+          <div class="section__inner">
+            <div class="logo">
+              <a class="logo-front" href="/">Harshit CHouhan</a>
+              <a class="logo-back" href="/">Back to Home</a>
+            </div>
+            <div class="main-menu">
+              <ul>
+                <li><a data-page="work.php" href="work.php">Work</a></li>
+                <li><a data-page="about.php" href="about.php">About</a></li>
+                <li><a data-page="contact.php" href="contact.php">Contact</a></li>
+                <!--                <li><a data-page="blog" href="blog">Blog</a></li>-->
+              </ul>
+            </div>
+            <div class="hamburger">
+              <span class="hamburger__top"></span>
+              <span class="hamburger__center"></span>
+              <span class="hamburger__bottom"></span>
+              <span class="hamburger__close-a"></span>
+              <span class="hamburger__close-b"></span>
+            </div>
+          </div>
+        </header>
+        <!-- header -->
+
+        <section class="section-content">
+          <div class="section__inner works">
+            <div class="swiper-container works__titles">
+              <div class="swiper-wrapper">
+                <div class="works__titles__item swiper-slide">
+                  <span><em>01</em>Explorica.com</span>
+                </div>
+                <div class="works__titles__item swiper-slide">
+                  <span><em>02</em>OaksWood.com</span>
+                </div>
+                <div class="works__titles__item swiper-slide">
+                  <span><em>03</em>Spoonify.com</span>
+                </div>
+                <div class="works__titles__item swiper-slide">
+                  <span><em>04</em>StoreToDoor</span>
+                </div>
+                <div class="works__titles__item swiper-slide">
+                  <span><em>05</em>Travelcations</span>
+                </div>
+                <div class="works__titles__item swiper-slide">
+                  <span><em>06</em>Random Meal Generator</span>
+                </div>
+                
+              </div>
+            </div>
+
+            <div class="swiper-container works__imgs rotate">
+              <div class="swiper-wrapper">
+                <div class="works__imgs__item swiper-slide">
+                  <div class="works__imgs__item__bg">
+                    <a class="work-link view-site" href="https://harshitchouhan.github.io/Explorica/index.html" target="_blank" data-color="#ff6845">
+                      <div class="works__imgs__item__bg__inner" style="background-image:url('img/works/explorica.png')"></div>
+                      <span>See project</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="works__imgs__item swiper-slide">
+                  <div class="works__imgs__item__bg">
+                    <a class="work-link view-site" href="https://harshitchouhan.github.io/OaksWood/" target="_blank" data-color="#4545a2">
+                      <div class="works__imgs__item__bg__inner" style="background-image:url('img/works/oakswood.png')"></div>
+                      <span>See project</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="works__imgs__item swiper-slide">
+                  <div class="works__imgs__item__bg">
+                    <a class="work-link view-site" href="https://harshitchouhan.github.io/Spoonify/" target="_blank" data-color="#d2314c">
+                      <div class="works__imgs__item__bg__inner" style="background-image:url('img/works/sppoonify.png')"></div>
+                      <span>See project</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="works__imgs__item swiper-slide">
+                  <div class="works__imgs__item__bg">
+                    <a class="work-link view-site" href="https://harshitchouhan.github.io/StoreToDoor/" target="_blank" data-color="#1c4360">
+                      <div class="works__imgs__item__bg__inner" style="background-image:url('img/works/StoreToDoor.png')"></div>
+                      <span>See project</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="works__imgs__item swiper-slide">
+                  <div class="works__imgs__item__bg">
+                    <a class="work-link view-site" href="https://harshitchouhan.github.io/Travelcations/" target="_blank" data-color="#6f20df">
+                      <div class="works__imgs__item__bg__inner" style="background-image:url('img/works/Travelcations.png')"></div>
+                      <span>See project</span>
+                    </a>
+                  </div>
+                </div>
+                <div class="works__imgs__item swiper-slide">
+                  <div class="works__imgs__item__bg">
+                    <a class="work-link view-site" href="https://harshitchouhan.github.io/RandomMealGenerator/" target="_blank" data-color="#464e65">
+                      <div class="works__imgs__item__bg__inner" style="background-image:url('img/works/Random Meal.png')"></div>
+                      <span>See project</span>
+                    </a>
+                  </div>
+                </div>
+
+                
+
+                
+              </div>
+              <span class="whity"></span>
+            </div>
+
+            <div class="alpha-zio">
+              <div class="alpha-zio__inner"><span></span></div>
+            </div>
+
+            <div class="works__controls">
+              <div class="works__pag"></div>
+              <div class="works__progress"><span></span></div>
+              <div class="works__nav">
+                <div class="works__nav__prev"></div>
+                <div class="works__nav__next"></div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <!-- footer -->
+        <footer class="section-footer">
+          <div class="section__inner">
+            <ul class="footer-social">
+              <li>
+                <a href="https://github.com/harshitchouhan" class="github" target="_blank" style="background-image: url(img/github.svg);"></a>
+              </li>
+
+              <li>
+                <a href="https://www.facebook.com/thatwebdev/" class="facebook" target="_blank"></a>
+              </li>
+
+              <li>
+                <a href="https://www.linkedin.com/in/harshit-chouhan-16a582179/" class="linkedin" target="_blank"></a>
+              </li>
+
+              <li>
+                <a href="https://www.instagram.com/that_webdev/?hl=en" class="instagram" target="_blank"></a>
+              </li>
+
+              <li>
+                <a href="https://twitter.com/Harshit52344484" class="twitter" target="_blank"></a>
+              </li>
+            </ul>
+            <span>© Harshit CHouhan</span>
+          </div>
+        </footer>
+        <!-- footer -->
+
+        <div class="work-bg preload"></div>
+      </div>
+    </div>
+    <div class="cursor">
+      <span class="cursor-alpha"></span>
+    </div>
+    <script>
+      (function(h) {
+        h.className = h.className.replace("no-js", "js");
+      })(document.documentElement);
+    </script>
+    <script src="assets/js/jquery.js" type="text/javascript"></script>
+    <script src="assets/js/main.js" type="text/javascript"></script>
+  </body>
+</html>
